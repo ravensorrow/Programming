0fb2373592dedcda220c8a7d996dcb44f9e34af1
Chris Brennan <xaero@xaerolimit.net>
2022-07-29 02:43:27 -0400

    Another Test Commit from Atom

--- Python/expression_examples.py
+++ Python/expression_examples.py
@@ -13,12 +13,11 @@ __author__ = "Christopher Brennan"
 __copyright__ = "Copyright 2022, The Grunk Project"
 __credits__ = ["Christopher Brennan", "Josh Cogliati"]
 __license__ = "GPL"
-__version__ = "0.1"
 __maintainer__ = "Christopher Brennan"
 __email__ = "chris@xaerolimit.net"
 __status__ = "Alpha Testing"
 
-import os, sys
+import os, sys, math
 
 #import <3rd party modules>
 
