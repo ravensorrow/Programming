7538a6cbfa32afea5ff09754c0710a1141d611d3
ravensorrow <chris@xaearolimit.net>
2022-12-31 16:33:05 -0500

    1. Python Commenting examples 2. versioneer to be removed on next commit, does not work correctly

---
+++ Python/Commenting.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env py3
+
+# Commenting can be done in two ways in python.
+# This way works well for inline comments with code.
+
+''' This is the preferred way to comment for documentation purposes.'''
+
+print("PI:  22 / 7 =", 22/7)    # An inaccurate but reasonable credible simulation of PI.
---
+++ Python/py_expressions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env py3
+
+''' Python3 Expressions'''
+
+print("Addition:            2 + 2 =", 2+2)
+print("Multiplication:      3 * 4 =", 3*4)
+print("Subtraction:         100 - 1 =", 100-1)
+print("Division:            14 / 3 =", 14/3)
+print("Interger Division:   14 // 3=", 14//3)
+print("Remainder (modulo):  14 % 3=", 14%3)
+print("(33 + 2) / 5 + 11.5 =🖕", (33+2)/5+11.5)
