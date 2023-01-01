307cd25106e6eb848f2a013a3be86fb4e28b225c
ravensorrow <chris@xaearolimit.net>
2022-12-31 20:25:02 -0500

    Stepping further through this book

---
+++ Python/Denmark.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env py3
+
+print("Something's rotten in the state of Denmark.")
+print("                -- Shakespeare")
---
+++ Python/halt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env py3
+
+print("Halt!")                              # Interrogation
+user_input = input("Who goes there? ")      # Response
+print("You may pass,", user_input)          # Resolution
