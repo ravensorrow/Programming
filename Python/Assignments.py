e7c65a9b9ce741cf9878f0143737d01050496cca
ravensorrow <chris@xaearolimit.net>
2022-12-31 20:25:02 -0500

    Stepping further through this book

---
+++ Python/Assignments.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env py3
+
+''' 
+    Oh hey! Look! We're gonna play with some assignments 
+'''
+
+a = 1
+print(a)
+a = a + 1
+print(a)
+a = a * 2
+print(a)
+
+# Print PI**2
+
+print("PI Spuared: ", (22/7)**2)
