1d8652ed29c3249825e0f3b37afd586c9b64585c
Christopher Brennan <chris@xaerolimit.net>
2023-10-08 21:01:36 -0400

    Finished first F to C converters, have some ideas to improve it too.

---
+++ Rust/f_to_c/target/debug/deps/f_to_c-43ce2e2a6c6adb8c.d
@@ -0,0 +1,5 @@
+/Volumes/MacData/Users/chris/Programming/Rust/f_to_c/target/debug/deps/f_to_c-43ce2e2a6c6adb8c: src/main.rs
+
+/Volumes/MacData/Users/chris/Programming/Rust/f_to_c/target/debug/deps/f_to_c-43ce2e2a6c6adb8c.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/f_to_c/target/debug/f_to_c.d
@@ -0,0 +1,1 @@
+/Volumes/MacData/Users/chris/Programming/Rust/f_to_c/target/debug/f_to_c: /Volumes/MacData/Users/chris/Programming/Rust/f_to_c/src/main.rs
