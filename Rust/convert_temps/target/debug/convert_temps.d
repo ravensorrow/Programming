07c2045adee85b811061f2d69431aa01c4a72afa
Christopher Brennan <chris@xaerolimit.net>
2023-10-08 23:07:54 -0400

    Added a second Temp Conversion project

---
+++ Rust/convert_temps/target/debug/convert_temps.d
@@ -0,0 +1,1 @@
+/home/grunk/Programming/Rust/convert_temps/target/debug/convert_temps: /home/grunk/Programming/Rust/convert_temps/src/main.rs
---
+++ Rust/convert_temps/target/debug/deps/convert_temps-3a7c2b2a283b9add.d
@@ -0,0 +1,5 @@
+/home/grunk/Programming/Rust/convert_temps/target/debug/deps/convert_temps-3a7c2b2a283b9add: src/main.rs
+
+/home/grunk/Programming/Rust/convert_temps/target/debug/deps/convert_temps-3a7c2b2a283b9add.d: src/main.rs
+
+src/main.rs:
