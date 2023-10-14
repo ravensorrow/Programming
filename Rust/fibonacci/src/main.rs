f6da5e450e99ff4effc8819eb5e4f5b5a8fa4ce5
Christopher Brennan <chris@xaerolimit.net>
2023-10-14 12:16:07 -0400

    Fibonacci sequence complete. I don't like this implimentation

--- Rust/fibonacci/src/main.rs
+++ Rust/fibonacci/src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+   println!("To quit the program, type 'quit'");
+    loop {
+        println!("Type a positive number now!");
+        let mut int = String::new();
+        io::stdin( )
+        .read_line(&mut int)
+        .expect("FAILURE! LOOSER! YOU FUCKED UP! TRY AGAIN! OR DONT!");
+        if int.trim() == "quit" {
+            println!("FUCKING LOOSER! FUCKING QUITTER! NO ONE WANTS YOU!");
+            break;
+        }
+        let int: u32 = match int.trim()
+        .parse() {
+            Ok(int) => int,
+            Err(_) => continue,
+        };
+    println!("Fibonacci ({}) => {}", int, fib(int.try_into().unwrap()));
+    }
+}
+
+fn fib (n: i32) -> i32 {
+    if n <= 0 {
+        return 0;
+    } else if n== 1 {
+        return 1;
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
 }
---
+++ Rust/fibonacci/target/debug/fibonacci.d
@@ -0,0 +1,1 @@
+/Volumes/MacData/Users/chris/Programming/Rust/fibonacci/target/debug/fibonacci: /Volumes/MacData/Users/chris/Programming/Rust/fibonacci/src/main.rs
