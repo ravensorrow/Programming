cdd46088ae7713e89300a952e90c1c62747f3df7
Christopher Brennan <chris@xaerolimit.net>
2023-10-22 21:52:19 -0400

    Starting Chapter 4 (Ownership)

--- Rust/ownership/src/main.rs
+++ Rust/ownership/src/main.rs
@@ -1,8 +1,15 @@
 fn main() {
-    {                       // 's' is not valid here, it's not yet decalred
-        let s = "Hello";    // 's' is valid from this point forward
-        //
-        // do some stuff with 's'
-    }                       //this scope is now over, and 's' is no lonher valid
-    println!(s);
+    let mut s = String::from("Hello");
+    s.push_str(", world!");             // push_str(); appends a literal to a String
+    println!("{}", s);                  // This will print 'hello world!'
+
+    // The code above works!
+    // The following code below will fail
+    let s1 = String::from("Hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    let x = 5;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
 }
