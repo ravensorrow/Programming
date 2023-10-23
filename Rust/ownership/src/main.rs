3aa422d9b7ae6f74957f58384440708c40095a03
Christopher Brennan <chris@xaerolimit.net>
2023-10-22 21:13:14 -0400

    Fixing Title
    Starting Chapter 4 (Ownership)

--- Rust/ownership/src/main.rs
+++ Rust/ownership/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    {                       // 's' is not valid here, it's not yet decalred
+        let s = "Hello";    // 's' is valid from this point forward
+        //
+        // do some stuff with 's'
+    }                       //this scope is now over, and 's' is no lonher valid
+    println!(s);
 }
---
+++ Rust/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Volumes/MacData/Users/chris/Programming/Rust/ownership/target/debug/ownership: /Volumes/MacData/Users/chris/Programming/Rust/ownership/src/main.rs
