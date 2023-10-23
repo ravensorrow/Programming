15482ee7cf6da0f1cd42cc5cb48f3b3b2cb9d332
Christopher Brennan <chris@xaerolimit.net>
2023-10-22 22:58:02 -0400

    Chapter 4.2: Ownership - References and Borrowing

---
+++ Rust/ownership/ownership_4_1/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Volumes/MacData/Users/chris/Programming/Rust/ownership/ownership_4_1/target/debug/ownership: /Volumes/MacData/Users/chris/Programming/Rust/ownership/ownership_4_1/src/main.rs
--- Rust/ownership/target/debug/ownership.d
+++
@@ -1,1 +0,0 @@
-/Volumes/MacData/Users/chris/Programming/Rust/ownership/target/debug/ownership: /Volumes/MacData/Users/chris/Programming/Rust/ownership/src/main.rs
