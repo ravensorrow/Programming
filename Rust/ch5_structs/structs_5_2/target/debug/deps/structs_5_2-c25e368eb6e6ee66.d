ebd5126942bb2fb3edbaa6cbf63d00825c670868
Christopher Brennan <chris@xaerolimit.net>
2024-01-25 23:55:55 -0500

    Finished Chapter 5.2

---
+++ Rust/ch5_structs/structs_5_2/target/debug/deps/structs_5_2-c25e368eb6e6ee66.d
@@ -0,0 +1,5 @@
+/Volumes/MacData/Users/chris/Programming/Rust/ch5_structs/structs_5_2/target/debug/deps/libstructs_5_2-c25e368eb6e6ee66.rmeta: src/main.rs
+
+/Volumes/MacData/Users/chris/Programming/Rust/ch5_structs/structs_5_2/target/debug/deps/structs_5_2-c25e368eb6e6ee66.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/ch5_structs/structs_5_2/target/debug/structs_5_2.d
@@ -0,0 +1,1 @@
+/Volumes/MacData/Users/chris/Programming/Rust/ch5_structs/structs_5_2/target/debug/structs_5_2: /Volumes/MacData/Users/chris/Programming/Rust/ch5_structs/structs_5_2/src/main.rs
