8ead29ccd6ea9562b09cf63efe6467b5ba076367
Christopher Brennan <chris@xaerolimit.net>
2024-01-30 20:17:55 -0500

    Finished 5.3. Final cimmit to chapter.

--- Rust/ch5_structs/structs_5_3/src/main.rs
+++ Rust/ch5_structs/structs_5_3/src/main.rs
@@ -9,60 +9,28 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
-    let rect = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
-        height: 50,
+        height: 30,
     };
 
-    println!(
-        "The area of the rectangle is {} spare pixles.",
-        rect.area()
-    );
-}
-
-// Example 2: more metod conversion
-struct Rectangle1 {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle1 {
-    fn width(&self) -> bool {
-        self.width > 0
-    }
-}
-
-fn main1() {
-    let rect1 = Rectangle1 {
-        width: 30,
-        height: 50,
-    };
-
-    if rect1.width() {
-        println!("The rectangle has the nonzero width; it is {}", rect1.width);
-    }
-}
-
-// Example 3: Metonds with MORE parameters!
-fn main2() {
     let rect2 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    
-    let rect3 = Rectangle {
         width: 10,
         height: 40,
     };
 
-    let rect4 = Rectangle {
+    let rect3 = Rectangle {
         width: 60,
         height: 45,
     };
 
-    println!("Can rect2 hold rect3? {}", rect2.can_hold(&rect3));
-    println!("Can rect1 hold rect3? {}", rect3.can_hold(&rect4));
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
--- Rust/ch5_structs/structs_5_3/target/debug/structs_5_3.d
+++ Rust/ch5_structs/structs_5_3/target/debug/structs_5_3.d
@@ -1,1 +1,1 @@
-/Volumes/MacData/Users/chris/Programming/Rust/ch5_structs/structs_5_3/target/debug/structs_5_3: /Volumes/MacData/Users/chris/Programming/Rust/ch5_structs/structs_5_3/src/main.rs
+/home/grunk/Programming/Rust/ch5_structs/structs_5_3/target/debug/structs_5_3: /home/grunk/Programming/Rust/ch5_structs/structs_5_3/src/main.rs
