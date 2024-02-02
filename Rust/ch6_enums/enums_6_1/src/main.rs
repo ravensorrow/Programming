2d0576368366ba2b912eec1a02f3f848868827e5
Christopher Brennan <chris@xaerolimit.net>
2024-02-01 19:56:45 -0500

    Knee deep in Ch 6.1

--- Rust/ch6_enums/enums_6_1/src/main.rs
+++ Rust/ch6_enums/enums_6_1/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+fn fn main() {
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
 }
+
+fn route(ip_kind: IpAddrKind) {}
+
+
---
+++ Rust/ch6_enums/enums_6_1/target/debug/enums_6_1.d
@@ -0,0 +1,1 @@
+/home/grunk/Programming/Rust/ch6_enums/enums_6_1/target/debug/enums_6_1: /home/grunk/Programming/Rust/ch6_enums/enums_6_1/src/main.rs
