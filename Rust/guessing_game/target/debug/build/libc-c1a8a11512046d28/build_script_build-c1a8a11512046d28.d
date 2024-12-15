e621db1f6a85b8bf68fd2c056f3cd2a34dbcdc0f
Christopher Brennan <chris@xaerolimit.net>
2024-12-14 23:47:03 -0500

    Delete Rust/guessing_game directory
    The project was restructured into a different directory.

--- Rust/guessing_game/target/debug/build/libc-c1a8a11512046d28/build_script_build-c1a8a11512046d28.d
+++
@@ -1,5 +0,0 @@
-/Volumes/MacData/Users/chris/Programming/Rust/guessing_game/target/debug/build/libc-c1a8a11512046d28/build_script_build-c1a8a11512046d28: /Volumes/MacData/Users/chris/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.147/build.rs
-
-/Volumes/MacData/Users/chris/Programming/Rust/guessing_game/target/debug/build/libc-c1a8a11512046d28/build_script_build-c1a8a11512046d28.d: /Volumes/MacData/Users/chris/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.147/build.rs
-
-/Volumes/MacData/Users/chris/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.147/build.rs:
