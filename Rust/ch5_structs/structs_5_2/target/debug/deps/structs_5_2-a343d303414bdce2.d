3e50521244107148141f02ba91221d9f9d431098
Christopher Brennan <chris@xaerolimit.net>
2024-12-14 23:38:29 -0500

    Delete Rust/ch5_structs directory
    The project was restructured into a new directory.

--- Rust/ch5_structs/structs_5_2/target/debug/deps/structs_5_2-a343d303414bdce2.d
+++
@@ -1,5 +0,0 @@
-/Volumes/MacData/Users/chris/Programming/Rust/ch5_structs/structs_5_2/target/debug/deps/structs_5_2-a343d303414bdce2: src/main.rs
-
-/Volumes/MacData/Users/chris/Programming/Rust/ch5_structs/structs_5_2/target/debug/deps/structs_5_2-a343d303414bdce2.d: src/main.rs
-
-src/main.rs:
--- Rust/ch5_structs/structs_5_3/target/debug/structs_5_3.d
+++
@@ -1,1 +0,0 @@
-/home/grunk/Programming/Rust/ch5_structs/structs_5_3/target/debug/structs_5_3: /home/grunk/Programming/Rust/ch5_structs/structs_5_3/src/main.rs
