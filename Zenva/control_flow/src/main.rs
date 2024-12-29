f4e26d267f61dbf293685a7cc07ab199d5a31d38
Christopher Brennan <chris@xaerolimit.net>
2024-12-28 22:24:42 -0500

    Control Flow lesson completed

--- Zenva/control_flow/src/main.rs
+++ Zenva/control_flow/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         Otherwise print 'Just another number'
     */
 
-    let number = 30;
+    let number: i32 = 30;
     if number % 5 == 0 && number % 3 == 0 {
         println!("{} is a TriQunit", number);
     } else if number % 6 == 0 && number % 4 == 0 {
@@ -14,27 +14,26 @@ fn main() {
         println!("{} is just another number", number);
     }
 
-    
     /*
         IF LET STATEMENT:
         If is_weekend is true, assign "Go Hiking" to the variable 'activity'
         otherwise, assign "Go to work"
-     */
+    */
 
-     let is_weekend:bool = true;
-     let activity = if is_weekend {"Go Hiking!"} else {"Go to work!"};
-     println!("{} is today's activity", activity);
+    let is_weekend:bool = true;
+    let activity: &str = if is_weekend {"Go Hiking!"} else {"Go to work!"};
+    println!("{} is today's activity", activity);
 
-     let is_weekend:bool = false;
-     let activity = if is_weekend {"Go Hiking!"} else {"Go to work!"};
-     println!("{} is today's activity", activity);
+    let is_weekend:bool = false;
+    let activity: &str = if is_weekend {"Go Hiking!"} else {"Go to work!"};
+    println!("{} is today's activity", activity);
 
-     /*
+    /*
         FOR LOOP
         Iterate over a collection like arrays
-     */
+    */
 
-    let arr = [10, 20, 30, 40, 50];
+    let arr: [i32; 5] = [10, 20, 30, 40, 50];
     for elem in arr {
         println!("{}", elem);
     }
@@ -44,8 +43,25 @@ fn main() {
         Print a count down and when counter reaches zero, print "LIFT OFF!"!
     */
 
-    let mut counter = 10;
+    let mut counter: i32 = 10;
     while counter > 0 {
-        println!("Countdown :{}", counter)
+        println!("Countdown :{}", counter);
+        counter -= 1;
+    }
+    println!("LIFT OFF!");
+
+    /*
+    LOOP STATEMENT
+    */
+
+    let mut index: i32 = 1;
+    loop {
+        index += 1;
+        println!("Index {}", index);
+
+        if index == 100 {
+            println!("Max index reached!");
+            break;
+        }
     }
 }
