1e9b7768ca31c67197cf714914d43bac1598071d
Christopher Brennan <chris@xaerolimit.net>
2024-12-29 17:24:09 -0500

    Fixed Fibonacci Sequence for 64-bit unsigned integers

--- Zenva/fibonacci_sequence/src/main.rs
+++ Zenva/fibonacci_sequence/src/main.rs
@@ -5,24 +5,22 @@ fn main() {
         0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . .
     */
 
-    let mut count = 0;
-    let mut a = 0;
-    let mut b = 1;
+    let mut count: u64 = 0;
+    let mut a: u64 = 0;
+    let mut b: u64 = 1;
 
-    loop {
-        if count  == 47 {
-            break;
-        }
-        if count == 0 {
+    loop { if count  == 50 
+        { 
+            break; 
+        } if count == 0 {
             println!("{}", a);
-        } else if  count == 1 {
+        } else if count == 1 {
             println!("{}", b);
         } else {
             let next = a + b;
             println!("{}", next);
             a = b;
             b = next;
-        }
-        count += 1;
+        } count += 1; 
     }
 }
