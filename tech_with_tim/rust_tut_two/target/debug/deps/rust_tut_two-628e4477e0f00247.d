0c8bfba3fecbe1856e741a9fa59132cb3fbcc5c7
Christopher Brennan <chris@xaerolimit.net>
2025-01-16 23:06:31 -0500

    Tech with Tim: Rust Tutorial #2

---
+++ tech_with_tim/rust_tut_two/target/debug/deps/rust_tut_two-628e4477e0f00247.d
@@ -0,0 +1,5 @@
+/home/grunk/Programming/tech_with_tim/rust_tut_two/target/debug/deps/rust_tut_two-628e4477e0f00247: src/main.rs
+
+/home/grunk/Programming/tech_with_tim/rust_tut_two/target/debug/deps/rust_tut_two-628e4477e0f00247.d: src/main.rs
+
+src/main.rs:
---
+++ tech_with_tim/rust_tut_two/target/debug/rust_tut_two.d
@@ -0,0 +1,1 @@
+/home/grunk/Programming/tech_with_tim/rust_tut_two/target/debug/rust_tut_two: /home/grunk/Programming/tech_with_tim/rust_tut_two/src/main.rs
